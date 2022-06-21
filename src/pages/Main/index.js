e43c0713f7f95585c8b21ea6d68ffea5c31f6def
cks612 <kyusung612@gmail.com>
2022-06-22 00:02:35 +0900

    isometric finished

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../../styles/_mainStyles.scss";
 
 const index = () => {
@@ -6,7 +7,14 @@ const index = () => {
     <div className="mainWrapper">
       <div className="mainContainer">
         <div className="items">
-          <div className="item"></div>
+          <div className="item">
+            <p>Isometric menu Tab</p>
+            <Link to={"/isometric"}>
+              <img src="/images/isometricMenu.png" alt="IsometricMenu" />
+            </Link>
+            <p>22. 06. 21</p>
+          </div>
+
           <div className="item"></div>
           <div className="item"></div>
           <div className="item"></div>
