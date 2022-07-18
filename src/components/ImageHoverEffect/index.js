ddab1a96b7c098fbe19ed48f0003c51f1ea009ee
cks612 <kyusung612@gmail.com>
2022-07-18 21:27:46 +0900

    imageHoverEffect added

---
+++ src/components/ImageHoverEffect/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styles/_imageHoverEffectStyles.scss";
+
+const index = () => {
+  return (
+    <div className="imageHoverWrapper">
+      <div className="box">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default index;
